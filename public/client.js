ff085c4438204857b1f8e3c549215248e69b797a
mahmoudmheisen91 <mahmoudmheisen91@gmail.com>
2020-04-17 15:00:01 +0300

    update client code

--- public/client.js
+++ public/client.js
@@ -1,98 +1,61 @@
 $(function () {
-  $("#newThread").submit(function () {
-    var board = $("#board1").val();
+  $("#testForm1").submit(function () {
+    let board = $(".board1").val();
     $(this).attr("action", "/api/threads/" + board);
   });
 
-  $("#newReply").submit(function () {
-    var board = $("#board4").val();
+  $("#testForm1a").submit(function () {
+    let board = $(".board1a").val();
     $(this).attr("action", "/api/replies/" + board);
   });
 
-  $("#reportThread").submit(function (e) {
-    var url = "/api/threads/" + $("#board2").val();
-    $.ajax({
-      type: "PUT",
-      url: url,
-      data: $(this).serialize(),
-      success: function (data) {
-        alert(data);
-      },
-    });
-    e.preventDefault();
-  });
-
-  $("#deleteThread").submit(function (e) {
-    var url = "/api/threads/" + $("#board3").val();
-    $.ajax({
-      type: "DELETE",
-      url: url,
-      data: $(this).serialize(),
-      success: function (data) {
-        alert(data);
-      },
-    });
-    e.preventDefault();
-  });
-
-  $("#reportReply").submit(function (e) {
-    var url = "/api/replies/" + $("#board5").val();
-    $.ajax({
-      type: "PUT",
-      url: url,
-      data: $(this).serialize(),
-      success: function (data) {
-        alert(data);
-      },
-    });
-    e.preventDefault();
-  });
-
-  $("#deleteReply").submit(function (e) {
-    var url = "/api/replies/" + $("#board6").val();
-    $.ajax({
-      type: "DELETE",
-      url: url,
-      data: $(this).serialize(),
-      success: function (data) {
-        alert(data);
-      },
-    });
-    e.preventDefault();
-  });
-  // ---------------------------------------------------------
-
-  $("#testForm1").submit(function (e) {
-    $.ajax({
-      url: "/api/issues/api_test",
-      type: "post",
-      data: $("#testForm1").serialize(),
-      success: function (data) {
-        $("#f2a").text(JSON.stringify(data));
-      },
-    });
-    e.preventDefault();
-  });
-
   $("#testForm2").submit(function (e) {
+    var url = "/api/threads/" + $(".board2").val();
     $.ajax({
-      url: "/api/issues/api_test",
-      type: "put",
-      data: $("#testForm2").serialize(),
+      type: "PUT",
+      url: url,
+      data: $(this).serialize(),
       success: function (data) {
-        $("#f2a").text(JSON.stringify(data));
+        alert(data);
+      },
+    });
+    e.preventDefault();
+  });
+
+  $("#testForm2a").submit(function (e) {
+    var url = "/api/threads/" + $(".board2a").val();
+    $.ajax({
+      type: "DELETE",
+      url: url,
+      data: $(this).serialize(),
+      success: function (data) {
+        alert(data);
       },
     });
     e.preventDefault();
   });
 
   $("#testForm3").submit(function (e) {
+    var url = "/api/replies/" + $(".board3").val();
     $.ajax({
-      url: "/api/issues/api_test",
-      type: "delete",
-      data: $("#testForm3").serialize(),
+      type: "PUT",
+      url: url,
+      data: $(this).serialize(),
       success: function (data) {
-        $("#f2a").text(JSON.stringify(data));
+        alert(data);
+      },
+    });
+    e.preventDefault();
+  });
+
+  $("#testForm3a").submit(function (e) {
+    var url = "/api/replies/" + $(".board3a").val();
+    $.ajax({
+      type: "DELETE",
+      url: url,
+      data: $(this).serialize(),
+      success: function (data) {
+        alert(data);
       },
     });
     e.preventDefault();
