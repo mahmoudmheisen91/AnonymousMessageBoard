0fec4219f1f0ee3d9a81ded6907ab1565cf197fb
mahmoudmheisen91 <mahmoudmheisen91@gmail.com>
2020-04-17 22:13:47 +0300

    add reply api

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # AnonymousMessageBoard
- Microservice for FCC Course 6, Project 5
+
+Microservice for FCC Course 6, Project 5
+
+## AnonymousMessageBoard
+
+FreeCodeCamp Information Security and Quality Assurance Course, Project 5
+
+Link to Glitch Server:
+https://mahmoudmheisen91-AnonymousMessageBoard.glitch.me/
--- routes/api.js
+++ routes/api.js
@@ -49,7 +49,13 @@ module.exports = function (app, db) {
               item.replies = item.replies.slice(0, 3);
             }
           });
-          res.json(docs);
+          if (err) {
+            res.status(400).type("text").send("could not find thread");
+            res.end();
+          } else {
+            res.json(docs);
+            res.end();
+          }
         });
     })
 
@@ -106,5 +112,119 @@ module.exports = function (app, db) {
       });
     });
 
-  app.route("/api/replies/:board");
+  app
+    .route("/api/replies/:board")
+    .post((req, res) => {
+      let board = req.params.board;
+      let reply = {
+        _id: new ObjectId(),
+        text: req.body.text,
+        deletepassword_: req.body.delete_password,
+        createdon_: new Date(),
+        reported: false,
+      };
+
+      let thread = {
+        _id: new ObjectId(req.body.thread_id),
+      };
+
+      db.collection(board).findAndModify(
+        thread,
+        [],
+        {
+          $set: { bumpedon_: new Date() },
+          $push: { replies: reply },
+        },
+        function (err, doc) {
+          if (err) {
+            res.redirect("/");
+          } else {
+            res.redirect("/b/" + board + "/");
+          }
+        }
+      );
+    })
+
+    .get((req, res) => {
+      let board = req.params.board;
+
+      let reply = {
+        _id: new ObjectId(req.query.thread_id),
+      };
+
+      db.collection(board)
+        .find(reply, {
+          reported: 0,
+          deletepassword_: 0,
+          "replies.reported": 0,
+          "replies.deletepassword_": 0,
+        })
+        .toArray(function (err, docs) {
+          if (err) {
+            res.status(400).type("text").send("could not find reply");
+            res.end();
+          } else {
+            res.json(docs[0]);
+            res.end();
+          }
+        });
+    })
+
+    .put((req, res) => {
+      let board = req.params.board;
+
+      let reply = {
+        _id: new ObjectId(req.body.thread_id),
+        "replies._id": new ObjectId(req.body.reply_id),
+      };
+
+      db.collection(board).findAndModify(
+        reply,
+        [],
+        { $set: { "replies.$.reported": true } },
+        function (err, doc) {
+          if (err) {
+            res
+              .status(400)
+              .type("text")
+              .send("could not update " + reply._id);
+            res.end();
+          } else {
+            res.send("success");
+            res.end();
+          }
+        }
+      );
+    })
+    .delete((req, res) => {
+      let board = req.params.board;
+
+      db.collection(board).findAndModify(
+        {
+          _id: new ObjectId(req.body.thread_id),
+          replies: {
+            $elemMatch: {
+              _id: new ObjectId(req.body.reply_id),
+              deletepassword_: req.body.delete_password,
+            },
+          },
+        },
+        [],
+        { $set: { "replies.$.text": "[deleted]" } },
+        function (err, doc) {
+          if (err) {
+            res.status(400).type("text").send("could not delete");
+            res.end();
+          } else {
+            if (doc.value === null) {
+              res.send("incorrect password");
+              res.end();
+            } else {
+              res.send("success");
+              res.end();
+            }
+          }
+        }
+      );
+    });
 };
